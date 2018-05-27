23cfdbd2bf1cc93ff58ff1328303050887fa2638
Malte Becker <maltebe@mail.uni-paderborn.de>
2018-05-27 11:43:56 +0200

    add menu in Main add reset function

--- app/src/main/java/de/tv1875paderborn/malte/badmintonspielbogen/MainActivity.java
+++ app/src/main/java/de/tv1875paderborn/malte/badmintonspielbogen/MainActivity.java
@@ -1,5 +1,6 @@
 package de.tv1875paderborn.malte.badmintonspielbogen;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -7,12 +8,65 @@ import android.os.Environment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_h3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_h1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_h2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_h3;
+
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd1_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_hd2_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_dd_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he1_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he2_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_he3_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_de_g3;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_g1;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_g2;
+import static de.tv1875paderborn.malte.badmintonspielbogen.ergebnis.er_mx_g3;
 
 public class MainActivity extends AppCompatActivity {
     // Allgemeine Daten speichern
@@ -96,6 +150,136 @@ public class MainActivity extends AppCompatActivity {
         ort = editText.getText().toString();
         editText = findViewById(R.id.editText_zeit);
         zeit = editText.getText().toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_save:
+                save();
+                return true;
+            case R.id.menu_reset:
+                reset();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void save(){
+
+    }
+    
+    final void reset(){
+        EditText editText = findViewById(R.id.editText_heimverein);
+        editText.setText("");
+        editText = findViewById(R.id.editText_gastverein);
+        editText.setText("");
+        editText = findViewById(R.id.editText_staffel);
+        editText.setText("");
+        editText = findViewById(R.id.editText_ort);
+        editText.setText("");
+        editText = findViewById(R.id.editText_zeit);
+        editText.setText("");
+
+        h_hd11 = "";
+        h_hd12 = "";
+        h_hd21 = "";
+        h_hd22 = "";
+        h_dd1 = "";
+        h_dd2 = "";
+        h_he1 = "";
+        h_he2 = "";
+        h_he3 = "";
+        h_de = "";
+        h_mx1 = "";
+        h_mx2 = "";
+        
+        g_hd11 = "";
+        g_hd12 = "";
+        g_hd21 = "";
+        g_hd22 = "";
+        g_dd1 = "";
+        g_dd2 = "";
+        g_he1 = "";
+        g_he2 = "";
+        g_he3 = "";
+        g_de = "";
+        g_mx1 = "";
+        g_mx2 = "";
+
+        erg_opend = false;
+        pdf_created = false;
+        
+        er_hd1_h1 = "";
+        er_hd1_h2 = "";
+        er_hd1_h3 = "";
+        er_hd2_h1 = "";
+        er_hd2_h2 = "";
+        er_hd2_h3 = "";
+        er_dd_h1 = "";
+        er_dd_h2 = "";
+        er_dd_h3 = "";
+        er_he1_h1 = "";
+        er_he1_h2 = "";
+        er_he1_h3 = "";
+        er_he2_h1 = "";
+        er_he2_h2 = "";
+        er_he2_h3 = "";
+        er_he3_h1 = "";
+        er_he3_h2 = "";
+        er_he3_h3 = "";
+        er_de_h1 = "";
+        er_de_h2 = "";
+        er_de_h3 = "";
+        er_mx_h1 = "";
+        er_mx_h2 = "";
+        er_mx_h3 = "";
+
+        er_hd1_g1 = "";
+        er_hd1_g2 = "";
+        er_hd1_g3 = "";
+        er_hd2_g1 = "";
+        er_hd2_g2 = "";
+        er_hd2_g3 = "";
+        er_dd_g1 = "";
+        er_dd_g2 = "";
+        er_dd_g3 = "";
+        er_he1_g1 = "";
+        er_he1_g2 = "";
+        er_he1_g3 = "";
+        er_he2_g1 = "";
+        er_he2_g2 = "";
+        er_he2_g3 = "";
+        er_he3_g1 = "";
+        er_he3_g2 = "";
+        er_he3_g3 = "";
+        er_de_g1 = "";
+        er_de_g2 = "";
+        er_de_g3 = "";
+        er_mx_g1 = "";
+        er_mx_g2 = "";
+        er_mx_g3 = "";
+
+        File myFile;
+        myFile = new File(this.getBaseContext().getExternalFilesDir(null), "/unterschriften/sign_heim.png");
+        if (myFile.exists()) {
+            myFile.delete();
+        }
+        myFile = new File(this.getBaseContext().getExternalFilesDir(null), "/unterschriften/sign_gast.png");
+        if (myFile.exists()) {
+            myFile.delete();
+        }
+
+        Toast.makeText(this.getBaseContext(), "Alle Eingaben gelöscht", Toast.LENGTH_LONG).show();
     }
 
     public void button_heimverein(View view) {
