361247fe09a12e0167920711856d2c66317a07a1
Malte Becker <maltebe@mail.uni-paderborn.de>
2018-08-14 15:05:58 +0200

    add load function change save function to make load usable

--- app/src/main/java/de/tv1875paderborn/malte/badmintonspielbogen/MainActivity.java
+++ app/src/main/java/de/tv1875paderborn/malte/badmintonspielbogen/MainActivity.java
@@ -1,10 +1,13 @@
 package de.tv1875paderborn.malte.badmintonspielbogen;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -185,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
                 save_json();
                 return true;
             case R.id.menu_load:
-                load_json();
+                choose_file();
                 return true;
             case R.id.menu_reset:
                 reset();
@@ -195,97 +199,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void save(){
-        String fileName = "testfile";
-        SharedPreferences sharedPreferences = getSharedPreferences(fileName, MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(getString(R.string.heimverein), heimverein);
-        editor.putString(getString(R.string.gastverein), gastverein);
-        editor.putString(getString(R.string.staffel), staffel);
-        editor.putString(getString(R.string.ort), ort);
-        editor.putString(getString(R.string.zeit), zeit);
-        editor.putString(getString(R.string.kommentar), kommentar);
-
-        editor.putString("h_hd11", h_hd11);
-        editor.putString("h_hd12", h_hd12);
-        editor.putString("h_hd21", h_hd21);
-        editor.putString("h_hd22", h_hd22);
-        editor.putString("h_dd1", h_dd1);
-        editor.putString("h_dd2", h_dd2);
-        editor.putString("h_he1", h_he1);
-        editor.putString("h_he2", h_he2);
-        editor.putString("h_he3", h_he3);
-        editor.putString("h_de", h_de);
-        editor.putString("h_mx1", h_mx1);
-        editor.putString("h_mx2", h_mx2);
-
-        editor.putString("g_hd11", g_hd11);
-        editor.putString("g_hd12", g_hd12);
-        editor.putString("g_hd21", g_hd21);
-        editor.putString("g_hd22", g_hd22);
-        editor.putString("g_dd1", g_dd1);
-        editor.putString("g_dd2", g_dd2);
-        editor.putString("g_he1", g_he1);
-        editor.putString("g_he2", g_he2);
-        editor.putString("g_he3", g_he3);
-        editor.putString("g_de", g_de);
-        editor.putString("g_mx1", g_mx1);
-        editor.putString("g_mx2", g_mx2);
-
-        editor.putString("er_hd1_h1", er_hd1_h1);
-        editor.putString("er_hd1_h2", er_hd1_h2);
-        editor.putString("er_hd1_h3", er_hd1_h3);
-        editor.putString("er_hd2_h1", er_hd2_h1);
-        editor.putString("er_hd2_h2", er_hd2_h2);
-        editor.putString("er_hd2_h3", er_hd2_h3);
-        editor.putString("er_dd_h1", er_dd_h1);
-        editor.putString("er_dd_h2", er_dd_h2);
-        editor.putString("er_dd_h3", er_dd_h3);
-        editor.putString("er_he1_h1", er_he1_h1);
-        editor.putString("er_he1_h2", er_he1_h2);
-        editor.putString("er_he1_h3", er_he1_h3);
-        editor.putString("er_he2_h1", er_he2_h1);
-        editor.putString("er_he2_h2", er_he2_h2);
-        editor.putString("er_he2_h3", er_he2_h3);
-        editor.putString("er_he3_h1", er_he3_h1);
-        editor.putString("er_he3_h2", er_he3_h2);
-        editor.putString("er_he3_h3", er_he3_h3);
-        editor.putString("er_de_h1", er_de_h1);
-        editor.putString("er_de_h2", er_de_h2);
-        editor.putString("er_de_h3", er_de_h3);
-        editor.putString("er_mx_h1", er_mx_h1);
-        editor.putString("er_mx_h2", er_mx_h2);
-        editor.putString("er_mx_h3", er_mx_h3);
-
-        editor.putString("er_hd1_g1", er_hd1_g1);
-        editor.putString("er_hd1_g2", er_hd1_g2);
-        editor.putString("er_hd1_g3", er_hd1_g3);
-        editor.putString("er_hd2_g1", er_hd2_g1);
-        editor.putString("er_hd2_g2", er_hd2_g2);
-        editor.putString("er_hd2_g3", er_hd2_g3);
-        editor.putString("er_dd_g1", er_dd_g1);
-        editor.putString("er_dd_g2", er_dd_g2);
-        editor.putString("er_dd_g3", er_dd_g3);
-        editor.putString("er_he1_g1", er_he1_g1);
-        editor.putString("er_he1_g2", er_he1_g2);
-        editor.putString("er_he1_g3", er_he1_g3);
-        editor.putString("er_he2_g1", er_he2_g1);
-        editor.putString("er_he2_g2", er_he2_g2);
-        editor.putString("er_he2_g3", er_he2_g3);
-        editor.putString("er_he3_g1", er_he3_g1);
-        editor.putString("er_he3_g2", er_he3_g2);
-        editor.putString("er_he3_g3", er_he3_g3);
-        editor.putString("er_de_g1", er_de_g1);
-        editor.putString("er_de_g2", er_de_g2);
-        editor.putString("er_de_g3", er_de_g3);
-        editor.putString("er_mx_g1", er_mx_g1);
-        editor.putString("er_mx_g2", er_mx_g2);
-        editor.putString("er_mx_g3", er_mx_g3);
-        editor.apply();
-    }
-
     public void save_json(){
+        onStop();
         Date datum = Calendar.getInstance().getTime();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.GERMANY);
         String date = simpleDateFormat.format(datum);
@@ -393,113 +308,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void load(){
-        String fileName = "testfile";
-        SharedPreferences sharedPref = getSharedPreferences(fileName, Context.MODE_PRIVATE);
-
-        heimverein = sharedPref.getString(getString(R.string.heimverein), null);
-        gastverein = sharedPref.getString(getString(R.string.gastverein), null);
-        staffel = sharedPref.getString(getString(R.string.staffel), null);;
-        ort = sharedPref.getString(getString(R.string.ort), null);
-        zeit = sharedPref.getString(getString(R.string.zeit), null);
-        kommentar = sharedPref.getString(getString(R.string.kommentar), null);
-
-        EditText editText = findViewById(R.id.editText_heimverein);
-        editText.setText(heimverein);
-        editText = findViewById(R.id.editText_gastverein);
-        editText.setText(gastverein);
-        editText = findViewById(R.id.editText_staffel);
-        editText.setText(staffel);
-        editText = findViewById(R.id.editText_ort);
-        editText.setText(ort);
-        editText = findViewById(R.id.editText_zeit);
-        editText.setText(zeit);
-        editText = findViewById(R.id.editText_kommentar);
-        editText.setText(kommentar);
-
-        h_hd11 = sharedPref.getString("h_hd11", null);
-        h_hd12 = sharedPref.getString("h_hd12", null);
-        h_hd21 = sharedPref.getString("h_hd21", null);
-        h_hd22 = sharedPref.getString("h_hd22", null);
-        h_dd1 = sharedPref.getString("h_dd1", null);
-        h_dd2 = sharedPref.getString("h_dd2", null);
-        h_he1 = sharedPref.getString("h_he1", null);
-        h_he2 = sharedPref.getString("h_he2", null);
-        h_he3 = sharedPref.getString("h_he3", null);
-        h_de = sharedPref.getString("h_de", null);
-        h_mx1 = sharedPref.getString("h_mx1", null);
-        h_mx2 = sharedPref.getString("h_mx2", null);
-
-        g_hd11 = sharedPref.getString("g_hd11", null);
-        g_hd12 = sharedPref.getString("g_hd12", null);
-        g_hd21 = sharedPref.getString("g_hd21", null);
-        g_hd22 = sharedPref.getString("g_hd22", null);
-        g_dd1 = sharedPref.getString("g_dd1", null);
-        g_dd2 = sharedPref.getString("g_dd2", null);
-        g_he1 = sharedPref.getString("g_he1", null);
-        g_he2 = sharedPref.getString("g_he2", null);
-        g_he3 = sharedPref.getString("g_he3", null);
-        g_de = sharedPref.getString("g_de", null);
-        g_mx1 = sharedPref.getString("g_mx1", null);
-        g_mx2 = sharedPref.getString("g_mx2", null);
-
-        er_hd1_h1 = sharedPref.getString("er_hd1_h1", null);
-        er_hd1_h2 = sharedPref.getString("er_hd1_h2", null);
-        er_hd1_h3 = sharedPref.getString("er_hd1_h3", null);
-        er_hd2_h1 = sharedPref.getString("er_hd2_h1", null);
-        er_hd2_h2 = sharedPref.getString("er_hd2_h2", null);
-        er_hd2_h3 = sharedPref.getString("er_hd2_h3", null);
-        er_dd_h1 = sharedPref.getString("er_dd_h1", null);
-        er_dd_h2 = sharedPref.getString("er_dd_h2", null);
-        er_dd_h3 = sharedPref.getString("er_dd_h3", null);
-        er_he1_h1 = sharedPref.getString("er_he1_h1", null);
-        er_he1_h2 = sharedPref.getString("er_he1_h2", null);
-        er_he1_h3 = sharedPref.getString("er_he1_h3", null);
-        er_he2_h1 = sharedPref.getString("er_he2_h1", null);
-        er_he2_h2 = sharedPref.getString("er_he2_h2", null);
-        er_he2_h3 = sharedPref.getString("er_he2_h3", null);
-        er_he3_h1 = sharedPref.getString("er_he3_h1", null);
-        er_he3_h2 = sharedPref.getString("er_he3_h2", null);
-        er_he3_h3 = sharedPref.getString("er_he3_h3", null);
-        er_de_h1 = sharedPref.getString("er_de_h1", null);
-        er_de_h2 = sharedPref.getString("er_de_h2", null);
-        er_de_h3 = sharedPref.getString("er_de_h3", null);
-        er_mx_h1 = sharedPref.getString("er_mx_h1", null);
-        er_mx_h2 = sharedPref.getString("er_mx_h2", null);
-        er_mx_h3 = sharedPref.getString("er_mx_h3", null);
-
-        er_hd1_g1 = sharedPref.getString("er_hd1_g1", null);
-        er_hd1_g2 = sharedPref.getString("er_hd1_g2", null);
-        er_hd1_g3 = sharedPref.getString("er_hd1_g3", null);
-        er_hd2_g1 = sharedPref.getString("er_hd2_g1", null);
-        er_hd2_g2 = sharedPref.getString("er_hd2_g2", null);
-        er_hd2_g3 = sharedPref.getString("er_hd2_g3", null);
-        er_dd_g1 = sharedPref.getString("er_dd_g1", null);
-        er_dd_g2 = sharedPref.getString("er_dd_g2", null);
-        er_dd_g3 = sharedPref.getString("er_dd_g3", null);
-        er_he1_g1 = sharedPref.getString("er_he1_g1", null);
-        er_he1_g2 = sharedPref.getString("er_he1_g2", null);
-        er_he1_g3 = sharedPref.getString("er_he1_g3", null);
-        er_he2_g1 = sharedPref.getString("er_he2_g1", null);
-        er_he2_g2 = sharedPref.getString("er_he2_g2", null);
-        er_he2_g3 = sharedPref.getString("er_he2_g3", null);
-        er_he3_g1 = sharedPref.getString("er_he3_g1", null);
-        er_he3_g2 = sharedPref.getString("er_he3_g2", null);
-        er_he3_g3 = sharedPref.getString("er_he3_g3", null);
-        er_de_g1 = sharedPref.getString("er_de_g1", null);
-        er_de_g2 = sharedPref.getString("er_de_g2", null);
-        er_de_g3 = sharedPref.getString("er_de_g3", null);
-        er_mx_g1 = sharedPref.getString("er_mx_g1", null);
-        er_mx_g2 = sharedPref.getString("er_mx_g2", null);
-        er_mx_g3 = sharedPref.getString("er_mx_g3", null);
-
+    private static String getFileExtension(File file) {
+        String fileName = file.getName();
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
     }
 
-    public void load_json(){
-        Date datum = Calendar.getInstance().getTime();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.GERMANY);
-        String date = simpleDateFormat.format(datum);
-        File file = new File(this.getBaseContext().getExternalFilesDir(null), date + "_" + "ein" + "-" + "test" + ".json");
+    private void choose_file(){
+
+        final ArrayList<String> l_jason = new ArrayList<>();
+        File file= new File(this.getBaseContext().getExternalFilesDir(null), "");
+        for (File f : file.listFiles()) {
+            if (f.isFile())
+                if (getFileExtension(f).equals("json"))
+                    l_jason.add(f.getName());
+        }
+
+        final CharSequence[] cs = l_jason.toArray(new CharSequence[l_jason.size()]);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wähle ein Spiel");
+        builder.setItems(cs, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String filename = cs[which].toString();
+                load_json(filename);
+            }
+        });
+        builder.show();
+    }
+
+    public void load_json(String filename){
+        File file = new File(this.getBaseContext().getExternalFilesDir(null), filename);
 
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
